44ac1cfa9c30e7f3eb20768ec9482bbeb053e900
Hamid <Hamid-K@users.noreply.github.com>
2026-01-03 23:45:59 +0100

    Fix import statements in indexFix index.tsx: add route loader and correct imports for Qwik build.tsx

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,24 +1,22 @@
-iimport { routeLoader$ } from '@builder.io/qwik-city';
+import { routeLoader$ } from '@builder.io/qwik-city';
 
 export const useRouteLoader = routeLoader$(() => {
   return {};
 });
-mport { component$, useContext } from '@builder.io/qwik';
-import { type DocumentHead } from "@builder.io/qwik-city";
+
+import { component$, useContext } from '@builder.io/qwik';
+import { type DocumentHead } from '@builder.io/qwik-city';
 
 import Hero from "~/components/furniture/hero";
 import SectionLinkGrid from "~/components/psc/section-link-grid";
 import Progress from "~/components/psc/progress";
 
 import { ChecklistContext } from '~/store/checklist-context';
-
 import { useChecklist } from '~/store/local-checklist-store';
 
-export default component$(() => { 
+export default component$(() => {
   const checklists = useContext(ChecklistContext);
-
   const localChecklist = useChecklist();
-
   return (
     <>
       <Hero />
